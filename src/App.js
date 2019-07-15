59ddc35c350b4d9d2ba44217302662e1f23a37f6
Seth Lewis <lewiscatenary@gmail.com>
2019-07-15 11:14:14 -0500

    added functions for home & away TDs

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ function App() {
   const [homeScore, setHomeScore] = useState(0);
   const [awayScore, setAwayScore] = useState(0);
 
+  const addHomeTD = () => {
+    setHomeScore(prevScore => prevScore + 7);
+  };
+  const addAwayTD = () => {
+    setAwayScore(prevScore => prevScore + 7);
+  };
   return (
     <div className="container">
       <section className="scoreboard">
@@ -30,11 +36,11 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
+          <button className="homeButtons__touchdown" onClick={addHomeTD}>Home Touchdown</button>
           <button className="homeButtons__fieldGoal">Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
+          <button className="awayButtons__touchdown" onClick={addAwayTD}>Away Touchdown</button>
           <button className="awayButtons__fieldGoal">Away Field Goal</button>
         </div>
       </section>
